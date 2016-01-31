a39b26928586a41d737c06bad8335da5175c0ddf
gummie <gudmundure12@ru.is>
2016-01-31 02:05:07 +0000

    Threading v2

--- main.py
+++ main.py
@@ -1,57 +1,46 @@
 from modules.sensorcontrol import SensorControl
 from modules.arduinocontrol import ArduinoControl
 from queue import Queue
-import  threading
+import threading
 import time
+
+exitFlag = 0
 
 # sens = SensorControl
 # ardc = ArduinoControl
 #
 # sens.run_sensors()
 # ardc.arduino_comm()
-#
-# print("Hallo")
 
-def process_data(threadName, q):
-    while not exitFlag:
-        Queue.queueLock.acquire()
-        if not workQueue.empty():
-            data = q.get()
-            Queue.queueLock.release()
-            print("%s processing %s" % (threadName, data))
-        else:
-            Queue.queueLock.release()
-        time.sleep(1)
+threadPool = Queue()
 
-threadList = ["Thread-1", "Thread-2", "Thread-3"]
-#threadList = [SensorControl]
-nameList = ["One", "Two", "Three", "Four", "Five"]
-queueLock = threading.Lock()
-workQueue = Queue(10)
-threads = []
-threadID = 1
+# for i in range(5):
+#     t = SensorControl(threadPool)
 
-# Create new threads
-for tName in threadList:
-    thread = SensorControl(threadID, tName, workQueue)
-    thread.start()
-    threads.append(thread)
-    threadID += 1
+sensorThread = SensorControl(threadPool)
+
+sensorThread.start()
+
+threadPool.put(SensorControl)
+
+print('Thread is alive:', sensorThread.is_alive())
+print("Number of active threads:", threading.active_count())
 
 # Fill the queue
-queueLock.acquire()
-for word in nameList:
-    workQueue.put(word)
-queueLock.release()
+# for i in range(5):
+#     threadPool.put(i)
+
+#queueLock.release()
 
 # Wait for queue to empty
-while not workQueue.empty():
-    pass
+# while not threadPool.empty():
+#     pass
 
 # Notify threads it's time to exit
-exitFlag = 1
+# exitFlag = 1
 
 # Wait for all threads to complete
-for t in threads:
-    t.join()
+# for t in threads:
+#     t.join()
+threadPool.join()
 print("Exiting Main Thread")
--- modules/sensorcontrol.py
+++ modules/sensorcontrol.py
@@ -2,31 +2,31 @@ from queue import Queue
 import threading
 import time
 
-exitFlag = 0
-
 class SensorControl(threading.Thread):
 
-    def __init__(self, threadID, name, q):
+    def __init__(self, queue):
         threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-        self.q = q
+        self.queue = queue
 
     def run(self):
         print("Starting " + self.name)
+        while True:
+            i = self.queue.get()
+            print(i)
+            self.queue.task_done()
         #run_sensors(self.name, self.q)
         print("Exiting " + self.name)
 
-    def run_sensors(threadname, q):
-        print("Run sensors")
-        while not exitFlag:
-            if not q.WokQueue.empty():
-                data = q.get()
-                q.queueLock.release()
-                print("%s processing %s" % (threadName, data))
-            else:
-                q.queueLock.release()
-            time.sleep(1)
+    # def run_sensors(threadname, queue):
+    #     print("Run sensors")
+    #     while not exitFlag:
+    #         if not queue.WokQueue.empty():
+    #             data = queue.get()
+    #             queue.queueLock.release()
+    #             print("%s processing %s" % (threadName, data))
+    #         else:
+    #             q.queueLock.release()
+    #         time.sleep(1)
 
 # def process_data(threadName, q, workQueue):
 #     while not exitFlag:
